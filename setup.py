cb8b71b70123251d492f031af8790af2ea28ff66
ronpandolfi <ronpandolfi@gmail.com>
2020-04-29 20:01:53 -0700

    Blackify

--- setup.py
+++ setup.py
@@ -2,47 +2,46 @@ from setuptools import setup, find_namespace_packages
 from codecs import open
 from os import path
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if x.startswith("git+")]
 
 setup(
-    name='xicam.CatalogViewer',
+    name="xicam.CatalogViewer",
     version=__version__,
-    description='',
+    description="",
     long_description=long_description,
-    url='',
-    license='BSD',
+    url="",
+    license="BSD",
     classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
     ],
-    keywords='Xi-cam, catalog',
-    packages=find_namespace_packages(exclude=['docs', 'tests*']),
+    keywords="Xi-cam, catalog",
+    packages=find_namespace_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
-    author='Ron Pandolfi',
+    author="Ron Pandolfi",
     install_requires=install_requires,
     dependency_links=dependency_links,
-    author_email='ronpandolfi@lbl.gov',
+    author_email="ronpandolfi@lbl.gov",
     entry_points={
-        'xicam.plugins.GUIPlugin': [
-            'catalog_viewer_plugin = xicam.catalog_viewer:CatalogViewerPlugin'],
-        'databroker.handlers': [
-            'JPEG = xicam.catalog_viewer.image_handlers:JPEGHandler',
-            'TIFF = xicam.catalog_viewer.image_handlers:TIFFHandler',
-            'EDF = xicam.catalog_viewer.image_handlers:EDFHandler'
-        ]
-        }
+        "xicam.plugins.GUIPlugin": ["catalog_viewer_plugin = xicam.catalog_viewer:CatalogViewerPlugin"],
+        "databroker.handlers": [
+            "JPEG = xicam.catalog_viewer.image_handlers:JPEGHandler",
+            "TIFF = xicam.catalog_viewer.image_handlers:TIFFHandler",
+            "EDF = xicam.catalog_viewer.image_handlers:EDFHandler",
+        ],
+    },
 )
--- xicam/catalog_viewer/__init__.py
+++ xicam/catalog_viewer/__init__.py
@@ -4,10 +4,11 @@ from xicam.plugins import GUIPlugin, GUILayout
 from xicam.gui.widgets.imageviewmixins import XArrayView, CatalogView
 import logging
 from xicam.core import msg
+
 # log = logging.getLogger('catalog_viewer')
 
-class CatalogViewerBlend(XArrayView, CatalogView):
 
+class CatalogViewerBlend(XArrayView, CatalogView):
     def __init__(self, *args, **kwargs):
         # CatalogViewerBlend inherits methods from XArrayView and CatalogView
         # super allows us to access both methods when calling super() from Blend
@@ -15,19 +16,20 @@ class CatalogViewerBlend(XArrayView, CatalogView):
 
 
 class CatalogViewerPlugin(GUIPlugin):
-    name = 'Catalog Viewer'
+    name = "Catalog Viewer"
+
     def __init__(self):
         self.layout = QHBoxLayout()
         self.parent_widget = QWidget()
         # add combobox to select stream
-        self.stream_label = QLabel('Select stream:')
+        self.stream_label = QLabel("Select stream:")
         self.stream_combo_box = QComboBox()
         self.stream_combo_box.setFixedWidth(150)
         self.layout.addWidget(self.stream_label)
         self.layout.addWidget(self.stream_combo_box)
         self.stream_combo_box.currentTextChanged.connect(self.stream_changed)
         # add combobox to select image in stream
-        self.field_label = QLabel('Select image:')
+        self.field_label = QLabel("Select image:")
         self.field_combo_box = QComboBox()
         self.field_combo_box.setFixedWidth(150)
         self.layout.addWidget(self.field_label)
@@ -38,8 +40,9 @@ class CatalogViewerPlugin(GUIPlugin):
         self.parent_widget.setLayout(self.layout)
         self.catalog_viewer = CatalogViewerBlend()
 
-        self.stages = {'Viewer': GUILayout(top=self.parent_widget, 
-                       center=self.catalog_viewer), }
+        self.stages = {
+            "Viewer": GUILayout(top=self.parent_widget, center=self.catalog_viewer),
+        }
         self.stream_fields = None  # list containing tuples of stream + field
         super(CatalogViewerPlugin, self).__init__()
 
@@ -58,10 +61,10 @@ class CatalogViewerPlugin(GUIPlugin):
                 return
             fields = self.stream_fields[new_stream]
             self.field_combo_box.clear()
-            self.field_combo_box.addItems(fields)           # needs a list of str
+            self.field_combo_box.addItems(fields)  # needs a list of str
             self.catalog_viewer.streamChanged(new_stream)
         except Exception as e:
-            msg.logError(e) 
+            msg.logError(e)
 
     def appendCatalog(self, run_catalog, **kwargs):
         try:
@@ -70,8 +73,8 @@ class CatalogViewerPlugin(GUIPlugin):
             self.stream_combo_box.clear()
             msg.showMessage(f"Loading primary image for {run_catalog.name}")
             # try and startup with primary catalog and whatever fields it has
-            if 'primary' in self.stream_fields:
-                default_stream_name = 'primary'
+            if "primary" in self.stream_fields:
+                default_stream_name = "primary"
             else:
                 default_stream_name = self.stream_fields.keys()[0]
             self.catalog_viewer.setCatalog(run_catalog, default_stream_name, None)
@@ -83,9 +86,10 @@ class CatalogViewerPlugin(GUIPlugin):
     def appendHeader(self):
         ...
 
+
 ### small helper functions
 def get_stream_data_keys(run_catalog, stream):
-    return run_catalog[stream].metadata['descriptors'][0]['data_keys']
+    return run_catalog[stream].metadata["descriptors"][0]["data_keys"]
 
 
 def get_all_streams(run_catalog):
@@ -93,26 +97,25 @@ def get_all_streams(run_catalog):
 
 
 def get_all_image_fields(run_catalog):
-    #image_fields = []
+    # image_fields = []
     all_streams_image_fields = {}
     for stream in get_all_streams(run_catalog):
         stream_fields = get_stream_data_keys(run_catalog, stream)
         field_names = stream_fields.keys()
         for field_name in field_names:
-            field_shape = len(stream_fields[field_name]['shape'])
+            field_shape = len(stream_fields[field_name]["shape"])
             if field_shape > 1 and field_shape < 5:
-                # if field contains at least 1 entry that is at least one-dimensional (shape=2) 
+                # if field contains at least 1 entry that is at least one-dimensional (shape=2)
                 # or 2-dimensional (shape=3) or up to 3-dimensional (shape=4)
                 # then add field e.g. 'fccd_image'
-                if stream in all_streams_image_fields.keys(): #add values to stream dict key
-                    all_streams_image_fields[stream].append(field_name) 
-                else: #if stream does not already exist in dict -> create new entry
-                    all_streams_image_fields[stream]=[field_name]
-            #TODO how to treat non image data fields in streams
-            #else:
+                if stream in all_streams_image_fields.keys():  # add values to stream dict key
+                    all_streams_image_fields[stream].append(field_name)
+                else:  # if stream does not already exist in dict -> create new entry
+                    all_streams_image_fields[stream] = [field_name]
+            # TODO how to treat non image data fields in streams
+            # else:
     return all_streams_image_fields
 
 
-
-#Problem: primary image field does not show up anymore...
-#Problem: primary image field does not show up anymore...
+# Problem: primary image field does not show up anymore...
+# Problem: primary image field does not show up anymore...
